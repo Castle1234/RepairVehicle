49ac09fdcfd61995631d2c3e5ccac0fd65d8f398
Castle1234 <149150815+Castle1234@users.noreply.github.com>
2024-02-21 11:39:11 +0000

    Add project files.

---
+++ RepairVehicle/Main.cs
@@ -0,0 +1,29 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+
+
+namespace RepairVehicle
+{
+    public class Main : BaseScript
+    {
+        [Command("repairvehicle")]
+        private void RepairVehicle()
+        {
+            
+            var localPlayer = Game.PlayerPed;
+            var localVehicle = localPlayer.CurrentVehicle;
+            
+
+            if (localVehicle != null)
+            {
+                API.SetVehicleFixed(localVehicle.Handle);
+                Screen.ShowNotification("Vehicle repaired.");
+            }
+            else
+            {
+                Screen.ShowNotification("You are not in a vehicle.");
+            }
+        }
+    }
+}
